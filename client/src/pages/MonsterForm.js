300ce73981a1e76d6172fe5ac2c85ef0725ab4a1
Jessica Nguyen <jessica.stevens2115@gmail.com>
2022-03-12 14:30:09 -0700

    added to read me

--- client/src/pages/MonsterForm.js
+++ client/src/pages/MonsterForm.js
@@ -34,7 +34,7 @@ const MonsterForm = () => {
         e.preventDefault()
         try {
             if (params.id) {
-                await axios.put(`/${params.id}`, {
+                await axios.put(`/api/monsters/${params.id}`, {
                     name,
                     id: params.id,
                 })
