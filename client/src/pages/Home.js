d05fd69977e1d6160c4aa4a600189c5b5314b2c1
Randy Clements <rcxlv@Randys-MacBook-Pro.local>
2022-03-12 14:24:37 -0800

    dataprovider complete in home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import Monster from '../components/Monster'
+import { DataContext } from '../providers/DataProvider'
 
 const Home = () => {
+    useEffect(()=>{
+        console.log("Use effect in home called")
+    },[])
+    const{ monsterData, deleteItem, deleteMonster} = useContext(DataContext)
+    const renderMonsterData = () => {
+        return monsterData.map((mon) =>{
+            return (
+                <div className='border' key={mon.monster.id} >
+                    <p>Monster Name: {mon.monster.name}</p>
+                    <p>Monster id: {mon.monster.id}</p>
+                    <button onClick={()=>deleteMonster(mon.monster.id)}>Delete Bug</button>
+                    <div className='border'>
+                    {mon.items.map((i) => {
+                        return(
+                            <div className='border' key={i.id}>
+                                <p>Name: {i.name}</p>
+                                <p> ID: {i.name}</p>
+                                <button onClick={()=>deleteItem(mon.monster.id, i.id)}>Delete treatment</button>
+                                </div>
+                        );
+                    })}
+                    </div>
+                </div>
+            )
+        })
+    }
     return (
         <div>
             <h1>Home</h1>
             <h2><Link to="/monsters">Monsters</Link></h2>
             <h2><Link to="/jobs">Jobs for Monsters</Link></h2>
             <h2><Link to="/about">About Us</Link></h2>
+            <div className="border">{renderMonsterData()}</div>
+            <hr />
+             <p className="border">{JSON.stringify(monsterData)}</p>
         </div>
     )
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
  namespace :api do
+  get 'all', to: 'monsters#all'
+  get 'monsters_all/:id', to: 'monsters#monsters_all'
+  get 'items',  to:'items#items_all'
   resources :monsters do
     resources :items
 end
