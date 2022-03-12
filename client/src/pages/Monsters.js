80b24b2777e30d2d6a30e0b1c542b72bc81e10d2
B <brendacassita@Brendas-MacBookPro.local>
2022-03-12 10:04:05 -0700

    get bugs works

--- client/src/pages/Monsters.js
+++ client/src/pages/Monsters.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
+import axios from 'axios'
 
 const Monsters = () => {
+    const navigate = useNavigate()
+    const [monsters, setMonsters] = useState([])
+    useEffect(()=>{
+        console.log('mounted and doing api call to get monsters')
+        getMonsters()
+    },[])
+
+    const getMonsters = async()=>{
+        let res = await axios.get('/api/monsters')
+        setMonsters(res.data)
+    }
+  
+
+
     return (
         <div>
             <h1>Monsters</h1>
-        </div>
+            <button onClick={()=>navigate('/monsters/new')}>new</button>
+            <div>
+                
+            </div>
+            <p>{JSON.stringify(monsters)}</p>
+            </div>
     )
 }
 
