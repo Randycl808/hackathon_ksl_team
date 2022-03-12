a1f8c68d3aa8f4509a4e2612c736e0253fdec3ff
Jessica Nguyen <jessica.stevens2115@gmail.com>
2022-03-12 09:59:23 -0700

    added DataProvider page

--- client/src/pages/MonsterForm.js
+++ client/src/pages/MonsterForm.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useContext, useState } from 'react'
+import { useParams, useLocation, useNavigate } from 'react-router-dom'
+import { DataContext } from '../providers/DataProvider'
 
 const MonsterForm = () => {
+    const params = useParams()
     return (
         <div className= 'border'>
             <h1>MonsterForm</h1>
+            <p>id: {params.id ? 'Edit' : 'New'}</p>
         </div>
     )
 }
