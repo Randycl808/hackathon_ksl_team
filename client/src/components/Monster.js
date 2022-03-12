4dd4dc681f049bd7b02fb926cd6bb4155672c712
B <brendacassita@Brendas-MacBookPro.local>
2022-03-12 11:13:35 -0700

    monsters crud almost finished

--- client/src/components/Monster.js
+++ client/src/components/Monster.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import {useNavigate, Link} from 'react-router-dom'
 
-const Monster = () => {
+const Monster = (props) => {
+    const navigate = useNavigate()
+    const {name, id, deleteMonster} = props
+
     return (
-        <div>
-            <h1>Monster</h1>
+        <div className= 'border'>
+        <h1>Monster</h1>
+        <p>id: {id}</p>
+        <p>name: {name}</p>
+            <Link to={`/monsters/${id}`} state={{name, id}}>show</Link>
+            <button onClick={()=> deleteMonster(id)}>delete</button>
+            <button onClick={()=> navigate(`monsters/${id}/edit`, {state: {name, id}})}>edit</button>
+            
         </div>
     )
 }
