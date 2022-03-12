12e0cf388271ab4feb1bbc3b9b28ccdd7e932024
Jessica Nguyen <jessica.stevens2115@gmail.com>
2022-03-12 11:29:47 -0700

    Links added on Home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
     return (
         <div>
             <h1>Home</h1>
+            <h2><Link to="/monsters">Monsters</Link></h2>
+            <h2><Link to="/jobs">Jobs for Monsters</Link></h2>
+            <h2><Link to="/about">About Us</Link></h2>
+            
+
+
         </div>
     )
 }
