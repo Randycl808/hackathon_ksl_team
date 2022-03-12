02b69ef700b1854bd06acc56c2f076fab9394108
Randy Clements <rcxlv@Randys-MacBook-Pro.local>
2022-03-11 16:21:25 -0800

    started working on the pages

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,6 +4,12 @@ const About = () => {
     return (
         <div>
             <h1>About</h1>
+            <p>MonSTARS</p>
+            <li>Brenda </li>
+            <li>Jess </li>
+            <li>Trina </li>
+            <li>Randy </li>
+            <p>This is our website for all types of Monsters. We sell Monster related items. We also provide our Monsters with jobs in our 'Jobs for Monsters' page.</p>
         </div>
     )
 }
