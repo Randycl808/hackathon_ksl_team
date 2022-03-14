12ec95be0ba380822a32c93992497b1669e77407
Brenda <brendacassita@gmail.com>
2022-03-14 14:47:53 -0600

    comments on job/monster pages

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -1,8 +1,14 @@
+// import React functionality
 import React from 'react'
+
+// import the useNavigate hook 
 import {useNavigate} from 'react-router-dom'
 
 const Job = (props) => {
+    // setting variable navigate in order to use useNavigate hook
     const navigate = useNavigate()
+
+    // passing down in order to use as props  
     const {title, id, company, salary, deleteJob} = props
 
     return (
@@ -15,7 +21,9 @@ const Job = (props) => {
 
             
             <button className="button" onClick={()=> deleteJob(id)}>delete</button>
+             {/* navigate hook gives the api route for edit in 1st argument and passes the state in 2nd argument in order to prepopulate content */}
             <button className="button" onClick={()=> navigate(`/jobs/${id}/edit`, {state: {title, id, company, salary}})}>edit</button>
+             {/* navigate hook gives the api route for show in 1st argument and passes the state in 2nd argument in order to prepopulate content */}
             <button className="button" onClick={()=> navigate(`/jobs/${id}`, {state: {title, id, company, salary}})}>show</button>
             
         </div>
--- client/src/components/Monster.js
+++ client/src/components/Monster.js
@@ -1,7 +1,12 @@
+// import React functionality
 import React from 'react'
+
+// import the useNavigate hook 
 import {useNavigate, } from 'react-router-dom'
 
 const Monster = (props) => {
+    // setting variable navigate in order to use useNavigate hook
+
     const navigate = useNavigate()
     const {name, id, deleteMonster} = props
 
@@ -12,7 +17,9 @@ const Monster = (props) => {
         <p>name: {name}</p>
             
             <button className="button" onClick={()=> deleteMonster(id)}>delete</button>
+             {/* navigate hook gives the api route for edit in 1st argument and passes the state in 2nd argument in order to prepopulate content */}
             <button className="button" onClick={()=> navigate(`/monsters/${id}/edit`, {state: {name, id}})}>edit</button>
+             {/* navigate hook gives the api route for show in 1st argument and passes the state in 2nd argument in order to prepopulate content */}
             <button className="button" onClick={()=> navigate(`/monsters/${id}`, {state: {name, id}})}>show</button>
             
         </div>
