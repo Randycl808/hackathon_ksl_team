52a9bb22c2a478b098b6814663d943438631821e
Brenda <brendacassita@gmail.com>
2022-03-14 18:10:19 -0600

    item form comments updated/ almost finished

--- client/src/components/ItemForm.js
+++ client/src/components/ItemForm.js
@@ -10,7 +10,10 @@ const ItemForm = (props) => {
     const [price, setPrice ] = useState(props.price || '');
     const [description, setDescription ] = useState(props.description || '');
 
+
+    // creating a function in order to use the handleSubmit effect
     const handleSubmit = (e)=>{
+        // prevents the default event from occurring
         e.preventDefault()
         console.log({name})
         if(props.id){
@@ -21,11 +24,17 @@ const ItemForm = (props) => {
     }
         return (
         <div className='border'>
+             {/* header for 'item form' */}
             <h1>ItemForm</h1>
+             {/* header for 'Edit' and 'New' item Form based on the params id, 
+             if there is an id populates data, if no id, blank form */}
             <h1>{props.id ? "EDIT" : "NEW"}</h1>
             <p>id: {props.id}</p>
+            {/* telling the handleSubmit to run when the form is submitted */}
             <form onSubmit={handleSubmit}>
                 <p>Name:</p>
+                {/* input field for name key, saying that with the onChange 
+                event it will set the Name to whatever input the user has entered */}
                 <input value={name} onChange={(e) => setName(e.target.value)} />
                 <p>Price:</p>
                 <input value={price} onChange={(e) => setPrice(e.target.value)} />
